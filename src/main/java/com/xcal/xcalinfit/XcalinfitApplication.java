0bbf94881cdfecf5f236a73bb609bad38a1df1d5
Tanapat Opal <tanapat.c@aware.co.th>
2019-04-03 09:51:44 +0700

    init

---
+++ src/main/java/com/xcal/xcalinfit/XcalinfitApplication.java
@@ -0,0 +1,13 @@
+package com.xcal.xcalinfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcalinfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcalinfitApplication.class, args);
+	}
+
+}
